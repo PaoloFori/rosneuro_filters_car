8f27cf67b42cd7f363d2ae723fee9cb52efdf1e0
PaoloFori <forin.paolo98@gmail.com>
2025-12-09 18:20:29 +0100

    add the possibility to remove some channels in the CAR

--- include/rosneuro_filters_car/Car.hpp
+++ include/rosneuro_filters_car/Car.hpp
@@ -3,6 +3,8 @@
 
 #include <Eigen/Dense>
 #include <gtest/gtest_prod.h>
+#include <vector>
+#include <algorithm>
 #include <rosneuro_filters/Filter.hpp>
 
 namespace rosneuro {
@@ -13,8 +15,14 @@ namespace rosneuro {
             ~Car(void) {};
 
             bool configure(void);
+            bool configure(std::vector<int> channels_exclude); 
+            
             DynamicMatrix<T> apply(const DynamicMatrix<T>& in);
             FRIEND_TEST(CarTestSuite, TestCarName);
+
+        private:
+            std::vector<int> eog_ch_excl_;
+            std::vector<bool> mask_excl_; 
     };
 
     template<typename T>
@@ -24,12 +32,60 @@ namespace rosneuro {
 
     template<typename T>
     bool Car<T>::configure(void) {
+        this->eog_ch_excl_.clear();
+        this->mask_excl_.clear();
+        return true;
+    }
+
+    template<typename T>
+    bool Car<T>::configure(std::vector<int> channels_exclude) {
+        this->eog_ch_excl_ = channels_exclude;
+        this->mask_excl_.clear();
+
         return true;
     }
 
     template<typename T>
     DynamicMatrix<T> Car<T>::apply(const DynamicMatrix<T>& in) {
-        return in - (in.rowwise().mean()).replicate(1, in.cols());
+        long n_samples = in.rows();
+        long n_channels = in.cols();
+
+        if (this->eog_ch_excl_.empty()) {
+            return in - (in.rowwise().mean()).replicate(1, n_channels);
+        }
+
+        if (this->mask_excl_.size() != n_channels) {
+            this->mask_excl_.assign(n_channels, false);
+            for (int idx : this->eog_ch_excl_) {
+                if (idx >= 0 && idx < n_channels) {
+                    this->mask_excl_[idx] = true;
+                }
+            }
+        }
+
+        Eigen::Matrix<T, Eigen::Dynamic, 1> common_average = Eigen::Matrix<T, Eigen::Dynamic, 1>::Zero(n_samples);
+        int valid_channels_count = 0;
+
+        for (long i = 0; i < n_channels; ++i) {
+            if (!this->mask_excl_[i]) {
+                common_average += in.col(i);
+                valid_channels_count++;
+            }
+        }
+
+        if (valid_channels_count > 0) {
+            common_average /= static_cast<T>(valid_channels_count);
+        } else {
+            return in;
+        }
+
+        DynamicMatrix<T> out = in; 
+
+        for (long i = 0; i < n_channels; ++i) {
+            out.col(i) -= common_average;
+        }
+        
+        return out;
     }
 }
 
